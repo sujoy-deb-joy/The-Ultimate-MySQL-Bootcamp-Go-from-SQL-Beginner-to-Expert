d17396de39899c1b854b5f82eb9e5e47033e3a8e
sujoydeb121 <sujoydebcoder121@gmail.com>
2024-04-04 14:58:31 +0200

    unique keyword example

---
+++ Constraints/unique_keyword_check.sql
@@ -0,0 +1,27 @@
+use bookshop;
+CREATE TABLE companies (
+    supplier_id INT AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    phone VARCHAR(15) NOT NULL UNIQUE,
+    address VARCHAR(255) NOT NULL,
+    PRIMARY KEY (supplier_id)
+);
+
+select * from companies;
+
+create table contacts (
+
+name varchar (50) not null,
+phone varchar (15) not null unique
+);
+
+insert into contacts (name, phone)
+values('joy', '9874563');
+insert into contacts (name, phone)
+-- values('setu', '9874563'); this will raise error as phone number can't be duplicated
+values('setu', '9874562');
+
+create table partiers (
+name varchar (40),
+age int check (age > 17)
+);
